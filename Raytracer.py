9fa3bdd9b24a750b2fb7f79605306b34870d7153
GonzaloSantizo <san21504@uvg.edu.gt>
2023-09-13 16:10:53 -0600

    final changes + snowman

--- Raytracer.py
+++ Raytracer.py
@@ -19,10 +19,29 @@ raytracer.rtClearColor(0.25,0.25,0.25)
 brick = Material(diffuse = (1, 0.4, 0.4), spec = 8)
 grass = Material(diffuse = (0.4, 1, 0.4), spec = 32)
 water = Material(diffuse = (0.4, 0.4, 1), spec = 256)
+Snow = Material(diffuse = (1,1,1), spec = 256)
+Coal = Material(diffuse = (0,0,0), spec = 256)
+Carrot = Material(diffuse = (1, 0.65, 0), spec = 256)
 
-raytracer.scene.append(Sphere(position=(-2,0,-5), radius = 0.5, material=brick))
-raytracer.scene.append(Sphere(position=(0,0,-5), radius = 0.5, material=grass))
-raytracer.scene.append(Sphere(position=(2,0,-5), radius = 0.5, material=water))
+
+#Body
+raytracer.scene.append(Sphere(position=(0,-1.5,-5), radius = 0.8, material=Snow))
+raytracer.scene.append(Sphere(position=(0,0,-5), radius = 0.8, material=Snow))
+raytracer.scene.append(Sphere(position=(0,1.5,-5), radius = 0.8, material=Snow))
+
+
+#eyes 
+raytracer.scene.append(Sphere(position=(-0.3,1.8,-5.3), radius = 0.2, material=Coal))
+raytracer.scene.append(Sphere(position=(0.3,1.6,-4.7), radius = 0.2, material=Coal))
+
+
+#Face
+raytracer.scene.append(Sphere(position=(-0.3,1.2,-5.3), radius = 0.1, material=Coal))
+raytracer.scene.append(Sphere(position=(0,1.0,-5), radius = 0.1, material=Coal))
+raytracer.scene.append(Sphere(position=(0.3,1.0,-4.7), radius = 0.1, material=Coal))
+
+#nose
+raytracer.scene.append(Sphere(position=(0,1.3,-5), radius = 0.1, material=Carrot))
 
 raytracer.lights.append(AmbientLight(intensity = 0.1))
 raytracer.lights.append(DirectionalLight(direction=(-1,-1,-1), intensity = 0.7))
